06129f29dc687b62cb63f7fddfa2d379270c974d
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-21 10:38:04 -0500

    condicional switch

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ let btn = document.querySelector("div");
 
 btn.addEventListener("click", (e)=>{
     let accion = e.target;
-    let html = ()=>{
-        let mensaje = document.querySelector("#mensaje");
-        mensaje.innerHTML = null;
-        mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
+    switch (accion.dataset.opc) {
+        case "consola":
+            console.log("Hola soy la opcion de la consola");
+            break;
+        case "ventena":
+            alert("Hola soy la opcion de la ventena");
+            break;
+        case "html":
+            let mensaje = document.querySelector("#mensaje");
+            mensaje.innerHTML = null;
+            mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
+            break;
+        case "emergente":
+            let ventana = document.querySelector("dialog");
+            if(accion.dataset.close) ventana.close();
+            else ventana.showModal();
+            break;
+        default:
+            alert("Opcion no encontrada");
+            break;
     }
-    let dialog = ()=>{
-        let ventana = document.querySelector("dialog");
-        if(accion.dataset.close) ventana.close();
-        else ventana.showModal();
-    }
-    (accion.localName == "button")
-        ? (accion.dataset.opc == "consola")
-            ? console.log("Hola soy la opcion de la consola")
-            : (accion.dataset.opc == "ventena") 
-                ? alert("Hola soy la opcion de la ventena")
-                : (accion.dataset.opc == "html") 
-                    ? html()
-                    : dialog()
-        : null;
 
     // if(accion.localName == "button"){
     //     if (accion.dataset.opc == "consola") console.log("Hola soy la opcion de la consola");
